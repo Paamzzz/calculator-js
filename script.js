bdedfeb3e651fa0a5d29a067bbd2f912280050a3
Paamzzz <pamamanciogoulart@gmail.com>
2026-03-08 00:19:49 -0300

    feat: calculadora finalizada com js

--- script.js
+++ script.js
@@ -1,17 +1,51 @@
-const displayValue = document.querySelector('#display').value;
+const display = document.querySelector('#display');
 const btns = document.querySelectorAll('.buttons button');
+
+let currentInput = '';
+let firstNumber = null;
+let operator = null;
 
 btns.forEach(btn => {
      btn.addEventListener('click', () => {
-         let type = btn.dataset.type;
-         let value = btn.dataset.value;
+         const type = btn.dataset.type;
+         const value = btn.dataset.value;
 
-          if (type === 'operation') {
+          if (type === 'number') {
+              currentInput += value;
+              display.value = currentInput;
 
-          } else if (type === 'number') {
-               
-          } else {
+          } else if (type === 'operation') {
+               if (currentInput === '') return; 
 
+               else if (firstNumber === null ) {
+                    firstNumber = Number(currentInput);
+               } else {
+                    firstNumber = calculate(firstNumber, Number(currentInput), operator)
+                    display.value = firstNumber
+               }
+
+               operator = value;
+               currentInput = ''
+
+          } else if (type === 'action') {
+               if (value === 'c') {
+                    currentInput = '';
+                    firstNumber = null;
+                    operator = null;
+                    display.value = '';
+               } else if (value === '=') {
+                    if (firstNumber !== null && currentInput !== '' && operator !== null) {
+                         const result = calculate(firstNumber, Number(currentInput), operator);
+                         display.value = result;
+                    }
+               }
           }
      });
 });
+
+function calculate(num1, num2, operator) {
+     if (operator === '+') return num1 + num2;
+     if (operator === '-') return num1 - num2;
+     if (operator === '*') return num1 * num2;
+     if (operator === '/') return num1 / num2;
+}
